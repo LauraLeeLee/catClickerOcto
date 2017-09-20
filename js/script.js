0d109046e6610ab758936d28a0524ab46787852b
Laura Logan <laurardh@aol.com>
2017-09-20 06:22:06 -0400

    adds files to git repo

---
+++ js/script.js
@@ -0,0 +1,30 @@
+//--------------Model
+var model = {
+  //array for crazy lady cat collection
+  var cats = [{
+    img: "images/catclicker.jpg",
+    name: "Kitty",
+    count: 0,
+    id: 1
+  },
+  { img: "images/catclicker2.jpg",
+    name: "Mr. Tipps",
+    count: 0,
+    id: 2
+  },
+  { img: "images/catclicker3.jpg",
+    name: "FraidyCat",
+    count: 0,
+    id: 3
+  },
+  { img: "images/catclicker4.jpg",
+    name: "MsClawz",
+    count: 0,
+    id: 4
+  },
+  { img: "images/catclicker5.jpg",
+    name: "ZsaZsa",
+    count: 0,
+    id:5
+  }];
+}
